619380b3350652ab81a42c873e34009de833b05d
congdt1997 <congjr97@gmail.com>
2020-03-26 11:59:14 +0700

    layout congdt

---
+++ resources/views/client/layout/index.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="client_asset/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="client_asset/assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="client_asset/assets/libs/css/style.css">
+    <link rel="stylesheet" href="client_asset/assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
+</head>
+
+<body>
+<!-- ============================================================== -->
+<!-- main wrapper -->
+<!-- ============================================================== -->
+<div class="dashboard-main-wrapper">
+    <!-- ============================================================== -->
+    <!-- navbar -->
+    <!-- ============================================================== -->
+    @include('client.layout.header')
+    <!-- ============================================================== -->
+    <!-- end navbar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- left sidebar -->
+    <!-- ============================================================== -->
+    @include('client.layout.menu')
+    <!-- ============================================================== -->
+    <!-- end left sidebar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- wrapper  -->
+    <!-- ============================================================== -->
+    <div class="dashboard-wrapper">
+    @yield('contentClient')
+        <!-- ============================================================== -->
+        <!-- end content -->
+        <!-- ============================================================== -->
+        <div class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="text-md-right footer-links d-none d-sm-block">
+                            <a href="javascript: void(0);">About</a>
+                            <a href="javascript: void(0);">Support</a>
+                            <a href="javascript: void(0);">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- end footer -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- end wrapper -->
+    <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- end main wrapper -->
+<!-- ============================================================== -->
+<!-- Optional JavaScript -->
+<!-- jquery 3.3.1  -->
+<script src="client_asset/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+<!-- bootstap bundle js -->
+<script src="client_asset/assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+<!-- slimscroll js -->
+<script src="client_asset/assets/vendor/slimscroll/jquery.slimscroll.js"></script>
+<!-- main js -->
+<script src="client_asset/assets/libs/js/main-js.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/client/layout/menu.blade.php
@@ -0,0 +1,59 @@
+<div class="nav-left-sidebar sidebar-dark">
+    <div class="menu-list">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="d-xl-none d-lg-none" href="#">Show Menu</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-divider">
+                        Menu
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="student-index.html" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1">
+                            <i class="fa fa-fw fa-user-circle"></i>Student
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="student-news.html" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4">
+                            <i class="fab fa-fw fa-wpforms"></i>News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="student-schedule.html" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5">
+                            <i class="fas fa-fw fa-table"></i>Schedules</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="student-message-chat.html" aria-expanded="false" data-target="#submenu-6" aria-controls="submenu-6">
+                            <i class="fab fa-facebook-messenger"></i> Message Chat </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-7" aria-controls="submenu-7">
+                            <i class="fas fa-fw fa-inbox"></i>Email</a>
+                        <div id="submenu-7" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="student-inbox.html">Inbox</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="student-email-details.html">Email Detail</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="student-email-compose.html">Email Compose</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="student-information-subject.html" aria-expanded="false" data-target="#submenu-8" aria-controls="submenu-8">
+                            <i class="fas fa-book"></i>Information Subject</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="student-information-class.html" aria-expanded="false" data-target="#submenu-9" aria-controls="submenu-9">
+                            <i class="fas fa-id-card-alt"></i>Information Class</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
